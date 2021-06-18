872b4f4984ca4e859a47844cbf62f7a5fdd0b228
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-17 19:53:57 -0700

    DemoPrint

---
+++ java.java
@@ -0,0 +1,12 @@
+
+package DemoPrint;
+
+public class java {
+    public static void main(String[] args) { 
+        System.out.println("Hello");
+        System.out.println("World");
+}
+}
+
+    
+
