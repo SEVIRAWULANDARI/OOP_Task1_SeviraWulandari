09e00aef8449d6380647a2977f7e84cd96f087f1
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:25:24 +0700

    cat

---
+++ cat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guided5;
+
+/**
+ *
+ * @author ACER
+ */
+public class cat {extends Animal {
+    public void name(){
+    System.out.println("Cat is one of animal name.");
+}
+
+}
+
+    
+
