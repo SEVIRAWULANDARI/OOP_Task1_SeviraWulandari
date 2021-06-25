d0527631750c1332d4f6fa0286c701b8b92b327d
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:29:17 +0700

    LuasKeliling

---
+++ LuasKeliling.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevira.unguided5;
+
+/**
+ *
+ * @author ACER
+ */
+public class LuasKeliling {
+    public static void main(String[] args) {
+     Shape shape = new Shape();
+        Square square = new Square(4);
+        Circle circle = new Circle(50);
+        Recatangle rectangle = new Recatangle(6,5);
+        
+        // memanggil method luas dan keliling
+        shape.area();
+        shape.circumference();
+        
+        System.out.println("Area of square: " + square.area());
+        System.out.println("Circumference of square: " + square.circumference());
+        System.out.println("Area of circle: " + circle.area());
+        System.out.println("Circumference of circle: " + circle.circumference());
+        System.out.println("Area of rectangle: " + rectangle.area());
+        System.out.println("Circumference of rectangle: " + rectangle.circumference());
+   
+    }
+}
+
+
