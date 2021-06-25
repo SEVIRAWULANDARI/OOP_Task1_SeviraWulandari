ba8e1a92a706da8c3c13a8ab6309d4f0e6d82bea
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:25:02 +0700

    animal1

---
+++ animal1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guided5;
+
+/**
+ *
+ * @author ACER
+ */
+public class animal1 {
+    public class Animal {
+    public void name(){
+    System.out.println("There are many kind of animal.");
+}
+
+}
+}
