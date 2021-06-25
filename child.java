d8c315ce29aba5a23a4a44203746d0ec593150e6
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:20:53 +0700

    child

---
+++ child.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevira.guided4a;
+
+/**
+ *
+ * @author ACER
+ */
+public class child extends Parent {
+    // NULL
+}
+
+
