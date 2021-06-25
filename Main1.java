27ed52ad3d46d5d7e40e784acce540cace55353f
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:24:24 +0700

    Main1

---
+++ Main1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevira.guided5;
+
+/**
+ *
+ * @author ACER
+ */
+public class Main1 {
+     public static void main(String[] args) {
+      Animal a = new Animal();
+      Animal b = new Cat();
+
+      // Call method in Animal class
+      a.name();
+      // Call method in Cat class
+      b.name();
+    }
+    
+}
+
+
