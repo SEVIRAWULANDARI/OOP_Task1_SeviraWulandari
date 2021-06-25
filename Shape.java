6ac0dc93ab32763a558a7cc95051cbc49b2fcb8e
SEVIRAWULANDARI <86089642+SEVIRAWULANDARI@users.noreply.github.com>
2021-06-25 08:30:14 +0700

    Shape

---
+++ Shape.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevira.unguided5;
+
+/**
+ *
+ * @author ACER
+ */
+public class Shape {
+    loat area(){
+        System.out.println("Calculate the area of shape");
+        return 0;
+    }
+    
+    float circumference(){
+        System.out.println("Calculate the circumference of shape");
+        return 0;
+    }
+}
+
+
